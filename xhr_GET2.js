de6409c8d4c3d7afbad0c30ccfa8c879f248c0fe
Thuillier Raynald <thuillier.raynald@gmail.com>
2021-05-27 09:18:06 +0200

    Add exercice

---
+++ xhr_GET2.js
@@ -0,0 +1,35 @@
+var form = document.getElementById('formId');
+form.addEventListener('submit', valider);
+
+function valider(event) {
+	event.preventDefault();	// bloque le comportement par défaut du navigateur (qui est de recharger la page)
+	
+	var elt = document.getElementById('reponse');
+	
+	var xhr = new XMLHttpRequest();
+
+	xhr.onreadystatechange = function() {
+		if (xhr.readyState === 4 && xhr.status == 200) {
+			elt.innerHTML = xhr.response ;
+		} else if (xhr.status == 404) {
+			elt.innerHTML = "PAGE INTROUVABLE" ;
+		}
+	}
+
+	// récupération des données du formulaire
+	var nom = document.getElementById('nom').value ;
+	var prenom = document.getElementById('prenom').value ;
+	var age = document.getElementById('age').value ;
+	
+	var data = 'nom=' + encodeURIComponent(nom) + '&prenom=' + encodeURIComponent(prenom) + '&age=' + encodeURIComponent(age) ;
+	// GET
+	xhr.open('GET', './xhr_GET2.php?' + data);
+	xhr.send();
+
+	// POST
+	/*
+	xhr.open('POST', '/test/taaest.php');
+	xhr.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded');
+	xhr.send(data)
+	*/
+}
---
+++ xhr_GET2.php
@@ -0,0 +1,7 @@
+<?php
+	$nom = $_GET["nom"];
+	$prenom = $_GET["prenom"];
+	$age = $_GET["age"];
+	
+	echo "Nom : $nom / Prénom : $prenom / Age : $age";
+?>
